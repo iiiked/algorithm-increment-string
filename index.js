b19c79ec1e8f7fd7a3de1abc1943316080e2a81f
Igor Snitkin <djshked@gmail.com>
2019-01-31 19:54:55 +0100

    Increment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function incrementString (str) {
-
+  if (!/\d+$/.test(str)) return str + '1';
+  return str.replace(/(\d+)$/, c => (Number(c) + 1).toString().padStart(c.length, '0'));
 }
 
 module.exports = incrementString;
